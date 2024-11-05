276e54913ce4c0778b16347eff6294a757f03026
TheGauravsahu <itsgauravsahu7@gmail.com>
2024-11-05 19:33:49 +0530

    Fix:Setting page update

--- src/app/(routes)/settings/page.tsx
+++ src/app/(routes)/settings/page.tsx
@@ -1,3 +1,4 @@
+import NavigateBack from "@/components/NavigateBack";
 import SettingsForm from "@/components/SettingsForm";
 import { getCurrentUserProfile } from "@/lib/data";
 import React from "react";
@@ -7,7 +8,10 @@ export default async function Settings() {
 
   return (
     <div className="p-8 w-full h-full">
-      <h1>Settings</h1>
+      <div className="flex gap-4 items-center">
+        <NavigateBack />
+        <h1>Settings</h1>
+      </div>
       <SettingsForm profile={profile} />
     </div>
   );
